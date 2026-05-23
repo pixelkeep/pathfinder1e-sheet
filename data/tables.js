4f16ee96d58c2a21e44a95cd05364221f9e9d520
pixelkeep <121554028+pixelkeep@users.noreply.github.com>
2026-05-23 20:37:59 +0200

    Add files via upload

--- data/tables.js
+++ data/tables.js
@@ -2,7 +2,7 @@
    Source: aonprd.com */
 'use strict';
 
-const SHEET_VERSION = '3.11.0';
+const SHEET_VERSION = '3.11.1';
 const SHEET_DATE    = '2026-05-17';
 
 // ── BAB PROGRESSIONS (per level 1-20) ─────────────
